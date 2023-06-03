ec574a142bdcf04c53cb947c8b766428f10f0f85
kshitiz <kshitiz.dhakal50@gmail.com>
2023-06-03 22:35:04 +0545

    analyses query is made for revenue by location

---
+++ hoteldbt/analyses/Revenue_by_HotelLocation.sql
@@ -0,0 +1,16 @@
+with dimhotel as (
+    select * from {{ ref('dim_hotels') }}
+),
+factbooking as (
+    select * from {{ ref('fct_bookings') }}
+)
+select 
+    dh.city,
+    fb.property_id,
+    sum(fb.revenue_realized) as Revenue
+from 
+    factbooking fb inner join dimhotel dh
+on 
+    fb.property_id=dh.property_id
+group by 
+    dh.city,fb.property_id;
