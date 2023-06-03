c592da94ad029100286210b00bd83804fdcb0670
kshitiz <kshitiz.dhakal50@gmail.com>
2023-06-03 21:44:59 +0545

    Surrogate key is generated using dbt-utils package

--- hoteldbt/models/dim/dim_hotels.sql
+++ hoteldbt/models/dim/dim_hotels.sql
@@ -1,7 +1,8 @@
 with src_hotels as (
     select * from {{ ref('src_hotels') }}
 )
-select 
+select
+    {{ dbt_utils.generate_surrogate_key(['PROPERTY_ID', 'PROPERTY_NAME', 'CATEGORY', 'CITY']) }} AS hotel_id,
     * 
 from 
     src_hotels
